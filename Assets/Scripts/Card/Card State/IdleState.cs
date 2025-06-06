3562c7986d92a4c9eee16dba582c57e763ee1db7
ThienVan1208 <ngvan12082005@gmail.com>
2025-06-06 19:47:23 +0700

    add moving effect in idle state.

--- Assets/Scripts/Card/Card State/IdleState.cs
+++ Assets/Scripts/Card/Card State/IdleState.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class IdleState : StateBase
 {
@@ -7,30 +8,40 @@ public class IdleState : StateBase
     private Vector2 rotate = Vector2.zero;
     private float _strenght = 10f;
     private float _speed = 1.5f;
-    public IdleState(Card card) : base( card)
+    private Vector3 _moveDir;
+    public IdleState(Card card) : base(card)
     {
     }
-    public override void OnEnter(){
+    public override void OnEnter()
+    {
         base.OnEnter();
         isComplete = true;
-        if(myCard.cardSlotRect != null) myCard.myRect.position = myCard.cardSlotRect.position;
-        _ranDir = Random.Range(-3f, 4f);
+        if (myCard.cardSlotRect != null) myCard.myRect.position = myCard.cardSlotRect.position;
+        _ranDir = Random.Range(-4f, 5f);
     }
 
     public override void OnExit()
     {
         base.OnExit();
+        // if (myCard.cardSlotRect != null) myCard.myRect.position = myCard.cardSlotRect.position;
     }
 
     public override void OnUpdate()
     {
         base.OnUpdate();
-        GetIdlde();
+        GetRotateEffect();
+        GetMoveEffect();
     }
-    private void GetIdlde(){
-
+    private void GetRotateEffect()
+    {
         rotate.x = Mathf.Sin((Time.realtimeSinceStartup + _ranDir) * _speed) * _strenght;
         rotate.y = Mathf.Cos((Time.realtimeSinceStartup + _ranDir) * _speed) * _strenght;
         myCard.myRect.localEulerAngles = rotate;
+    }
+    private void GetMoveEffect()
+    {
+        _moveDir.y = Mathf.Sin(Time.realtimeSinceStartup + _ranDir) / 50f;
+        // Debug.Log(_moveDir.y);
+        myCard.myRect.localPosition += _moveDir;
     }
 }
